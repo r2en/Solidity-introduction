e3a0e9e2a0753d8fb5f02423d268d27e1a1255f8
xxxHAL <s12161te@gmail.com>
2018-06-26 12:06:26 +0900

    add:

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -8,6 +8,9 @@ contract ZombieFeeding is ZombieFactory {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     // ここから開始せよ
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    _createZombie("NoName", newDna);
   }
 
 }
