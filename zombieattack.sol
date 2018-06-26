775645516cdb3de5944c09e2f13d74ac76bfb243
xxxHAL <s12161te@gmail.com>
2018-06-26 19:56:13 +0900

    add: 乱数生成

---
+++ zombieattack.sol
@@ -0,0 +1,10 @@
+import "./zombiehelper.sol";
+contract ZombieBattle is ZombieHelper {
+  uint randNonce = 0;
+
+  function randMod(uint _modulus) internal returns(uint) {
+    randNonce++;
+    return uint(keccak256(now, msg.sender, randNonce)) % _modulus;
+  }
+}
+
