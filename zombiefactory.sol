a8e1948d0ad91e576cf02702035b3d2dd8c78973
xxxHAL <s12161te@gmail.com>
2018-06-26 14:16:26 +0900

    add: ownable construct

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.4.19;
 
+// 1. ここにインポートせよ
+
+// 2. ここで継承せよ:
 contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
@@ -17,7 +20,7 @@ contract ZombieFactory {
     mapping (uint => address) public zombieToOwner;
     mapping (address => uint) ownerZombieCount;
 
-    function _createZombie(string _name, uint _dna) private {
+    function _createZombie(string _name, uint _dna) internal {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
@@ -32,6 +35,7 @@ contract ZombieFactory {
     function createRandomZombie(string _name) public {
         require(ownerZombieCount[msg.sender] == 0);
         uint randDna = _generateRandomDna(_name);
+        randDna = randDna - randDna % 100;
         _createZombie(_name, randDna);
     }
 
