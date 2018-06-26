a4dcc4bf6a620647d4313ffcf6d8871bb41bb4e4
xxxHAL <s12161te@gmail.com>
2018-06-26 16:30:56 +0900

    modify:

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -4,9 +4,20 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
-  // ここから開始せよ
-  modifier aboveLevel(uint _level, uint _zombieId){
+  modifier aboveLevel(uint _level, uint _zombieId) {
     require(zombies[_zombieId].level >= _level);
     _;
   }
+
+  // ここから開始せよ
+  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].name = _newName;
+  }
+
+  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId){
+    require(msg.sender == zombieToOwner[_zombieId]);
+    zombies[_zombieId].dna = _newDna;
+  }
+
 }
